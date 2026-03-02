5ddbb30c24b163d19aa6cd7d48fd2562316bfe78
Mubashir <mubashirmuchi75@gmail.com>
2026-03-02 23:24:15 +0530

    commit

---
+++ lib/strapi.ts
@@ -0,0 +1,28 @@
+const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
+const STRAPI_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+
+if (!STRAPI_URL) {
+  throw new Error("NEXT_PUBLIC_STRAPI_URL is missing or empty");
+}
+
+export async function strapiFetch(path: string, 
+    // options = {}
+) {
+  const res = await fetch(`${STRAPI_URL}${path}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...(STRAPI_TOKEN && {
+        Authorization: `Bearer ${STRAPI_TOKEN}`,
+      }),
+    },
+    // cache: options.cache ?? "no-store",
+  });
+
+  if (!res.ok) {
+    const text = await res.text();
+    throw new Error(`Strapi ${res.status}: ${text}`);
+  }
+
+  return res.json();
+}
+
